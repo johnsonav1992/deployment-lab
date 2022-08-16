c03e6e4fc1462cd946633bfb14184fb8c0171de9
Alex Johnson <johnsonav1992@gmail.com>
2022-08-16 14:00:29 -0500

    init commit

---
+++ server/controller.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+    getHomepage: (req, res) => {
+            res.sendFile(path.join(__dirname, '../../client/index.html'))
+    },
+
+    getCSS: (req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/styles.css'))
+    },
+
+    getJS: (req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/main.js'))
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const controller = require('./controller.js');
+const {getHomepage, getCSS, getJS} = controller;
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+port = process.env.PORT || 4000;
+
+app.get('/', getHomepage);
+app.get('/css',getCSS);
+app.get('/js', getJS);
+app.listen(port,console.log(`Server running on ${port}`))
+

77f5803e6f69e87bf6f2bbebb1500b2d6e659061
Alex Johnson <johnsonav1992@gmail.com>
2022-08-16 14:09:54 -0500

    fix file paths controller

--- server/controller.js
+++ server/controller.js
@@ -2,14 +2,14 @@ const path = require('path')
 
 module.exports = {
     getHomepage: (req, res) => {
-            res.sendFile(path.join(__dirname, '../../client/index.html'))
+            res.sendFile(path.join(__dirname, '../client/index.html'))
     },
 
     getCSS: (req, res) => {
-        res.sendFile(path.join(__dirname, '../../client/styles.css'))
+        res.sendFile(path.join(__dirname, '../client/styles.css'))
     },
 
     getJS: (req, res) => {
-        res.sendFile(path.join(__dirname, '../../client/main.js'))
+        res.sendFile(path.join(__dirname, '../client/main.js'))
     }
 }

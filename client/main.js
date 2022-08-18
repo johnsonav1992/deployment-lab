a394deba9e17407d3b1fec690bc0ce061a99b80f
Alex Johnson <johnsonav1992@gmail.com>
2022-08-18 14:37:58 -0500

    repush

--- client/main.js
+++ client/main.js
@@ -1,11 +1,4 @@
-const Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-    accessToken: '3aaa61765f034ab38a174ebc1cbb3f7f',
-    captureUncaught: true,
-    captureUnhandledRejections: true
-  });
-
-rollbar.warning('WARNING!!!!!!')
+Rollbar.warning('WARNING!!!!!!')
 
 const homeLink = document.querySelector('.home-link')
 
--- server/controller.js
+++ server/controller.js
@@ -1,12 +1,3 @@
-const path = require('path')
-const Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-    accessToken: 'ea570cc7d063423994fb96d4b27f1067',
-    captureUncaught: true,
-    captureUnhandledRejections: true
-  });
-
-
 
 module.exports = {
     getHomepage: (req, res) => {
@@ -26,7 +17,7 @@ module.exports = {
         try {
             doesNotExist()
         } catch (err) {
-            rollbar.critical(err)
+            Rollbar.critical(err)
         }
     }
 

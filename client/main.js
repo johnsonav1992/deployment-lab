0bf856aad3970739572f5ac085032ed4bccab1e4
Alex Johnson <johnsonav1992@gmail.com>
2022-08-18 13:41:29 -0500

    added warning

---
+++ client/main.js
@@ -0,0 +1,12 @@
+const Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+    accessToken: 'f6fa480d554e4760ac11ba0f768be238',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+  });
+
+const homeLink = document.querySelector('.home-link')
+
+homeLink.addEventListener('click', () => {
+    rollbar.warn('WARNING!!!!!!')
+})

c709ce67a660995304a568a4256f800b1744449e
Alex Johnson <johnsonav1992@gmail.com>
2022-08-18 13:30:31 -0500

    no static

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ rollbar.log("Hello world!");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('client'))
+// app.use(express.static('client'))
 
 const port = process.env.PORT || 4000;
 

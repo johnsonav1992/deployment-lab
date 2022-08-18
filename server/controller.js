0a5098b76df26c0d41642910c28522264b00a1c5
Alex Johnson <johnsonav1992@gmail.com>
2022-08-18 13:32:57 -0500

    put everything back

--- server/controller.js
+++ server/controller.js
@@ -13,10 +13,10 @@ try {
 }
 
 module.exports = {
-    // getHomepage: (req, res) => {
-    //     res.sendFile(path.join(__dirname, '../client/index.html'))
+    getHomepage: (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/index.html'))
            
-    // },
+    },
 
     getCSS: (req, res) => {
         res.sendFile(path.join(__dirname, '../client/styles.css'))
--- server/server.js
+++ server/server.js
@@ -18,11 +18,11 @@ rollbar.log("Hello world!");
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.static('client'))
+app.use(express.static('client'))
 
 const port = process.env.PORT || 4000;
 
-// app.get('/', getHomepage);
+app.get('/', getHomepage);
 app.get('/rollbar', getRollbar);
 app.get('/css',getCSS);
 app.get('/js', getJS);

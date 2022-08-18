03b1a013c60e7d68e0f3d53bec4031bfeb1fb9d7
Alex Johnson <johnsonav1992@gmail.com>
2022-08-18 14:43:11 -0500

    rollbar in button

--- client/main.js
+++ client/main.js
@@ -1,7 +1,6 @@
-Rollbar.warning('WARNING!!!!!!')
-
 const homeLink = document.querySelector('.home-link')
 
 homeLink.addEventListener('click', (e) => {
     e.preventDefault()
+    Rollbar.warning('WARNING!!!!!!')
 })
